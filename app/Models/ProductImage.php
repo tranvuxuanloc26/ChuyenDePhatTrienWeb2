ef5f161df6a5b2968434ae07ed3e08b94b107911
PhamLeMinhTinh <21211tt1504@mail.tdc.edu.vn>
2024-10-18 21:53:51 +0700

    chore: add database

---
+++ app/Models/ProductImage.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductImage extends Model
+{
+    use HasFactory;
+    protected $table = 'product_images';
+
+    protected $fillable = [
+        'name',
+        'pro_id',
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(related: Product::class, foreignKey: 'pro_id');
+    }
+}
