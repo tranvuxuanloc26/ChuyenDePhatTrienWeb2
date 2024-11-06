0bcf987370369a8547f0dc3fcd411adf34910067
lethanhhung2003 <gac0n10kg@gmail.com>
2024-11-07 02:03:09 +0700

    quen_mat_khau

---
+++ app/Http/Controllers/Admin/Users/UserController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Users;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Services\User\UserService;
+
+
+class UserController extends Controller
+{
+    protected $user;
+    public function __construct(UserService $user){
+        $this->user = $user;
+    }
+   
+      public function index (){
+        return view('admin.users.list', [
+             'title' => 'Danh Sách user Mới Nhất',
+             'users' => $this->user->get()
+        ]);
+    }
+  
+   
+    public function destroy(Request $request){
+        $result = $this->user->destroy($request);
+        if($result){
+            return response()->json([
+                'error' => false,
+                'message' => 'Xoá thành công user'
+            ]);
+
+            
+        }
+        return response()->json(['error' => true]);
+    }
+
+    public function toggleRole($id)
+    {
+        // Tìm kiếm user theo ID
+        $user = User::find($id);
+        if ($user) {
+            // Chuyển đổi role: Nếu role là 1 thì đổi thành 0, ngược lại đổi thành 1
+            $user->role = $user->role == 0 ? 1 : 0;
+            $user->save();
+
+            return response()->json(['success' => true]);
+        }
+
+        return response()->json(['success' => false], 404);
+    }
+}
---
+++ app/Http/Controllers/ProductControllerScreen.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use App\Http\Services\Product\ProductServiceScreen;
+
+class ProductControllerScreen extends Controller
+{
+     protected $productServiceScreen;
+     public function __construct(ProductServiceScreen $productServiceScreen)
+     {
+          $this->productServiceScreen = $productServiceScreen;
+     }
+     public function index($id = '', $slug = '')
+     {
+              $product = $this->productServiceScreen->show($id);
+              $productMore = $this->productServiceScreen->more($id);
+
+              return view('product.content', [
+                   'title' => $product->name,
+                   'product' => $product,
+                   'products' => $productMore,
+
+
+              ]);
+     }
+
+     // public function indexQuickView($id = '', $slug = '')
+     // {
+     //      $product = $this->productServiceScreen->show($id);
+         
+
+     //      $productMore = $this->productServiceScreen->more($id);
+
+     //      return view('product.popup', [
+     //           'title' => $product->name,
+     //           'product' => $product,
+     //           'products' => $productMore
+
+     //      ]);
+     // }
+     public function getFeaturedProducts()
+{
+    $featuredProducts = Product::where('is_featured', true)->get();
+
+    // Trả về partial view chứa danh sách sản phẩm nổi bật
+    return view('product.featured_products', compact('featuredProducts'))->render();
+}
+
+public function getNewestProducts(Request $request) {
+     $products = Product::orderBy('updated_at', 'desc')->paginate(16); // Lấy sản phẩm mới nhất theo thời gian tạo
+ 
+     return view('product.newest_products',[
+          'title' => 'Sản phẩm mới nhất'
+     ], compact('products'));
+ }
+     
+
+}
