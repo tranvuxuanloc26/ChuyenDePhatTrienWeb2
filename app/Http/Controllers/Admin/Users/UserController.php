7dd71f04f27ece5f6cd916e35e328c8f838055ce
lethanhhung2003 <gac0n10kg@gmail.com>
2024-11-07 05:39:41 +0700

    profile

---
+++ app/Http/Controllers/Admin/Users/UserController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Users;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Services\User\UserService;
+
+
+class UserController extends Controller
+{
+    protected $user;
+    public function __construct(UserService $user){
+        $this->user = $user;
+    }
+   
+      public function index (){
+        return view('admin.users.list', [
+             'title' => 'Danh Sách user Mới Nhất',
+             'users' => $this->user->get()
+        ]);
+    }
+  
+   
+    public function destroy(Request $request){
+        $result = $this->user->destroy($request);
+        if($result){
+            return response()->json([
+                'error' => false,
+                'message' => 'Xoá thành công user'
+            ]);
+
+            
+        }
+        return response()->json(['error' => true]);
+    }
+
+    public function toggleRole($id)
+    {
+        // Tìm kiếm user theo ID
+        $user = User::find($id);
+        if ($user) {
+            // Chuyển đổi role: Nếu role là 1 thì đổi thành 0, ngược lại đổi thành 1
+            $user->role = $user->role == 0 ? 1 : 0;
+            $user->save();
+
+            return response()->json(['success' => true]);
+        }
+
+        return response()->json(['success' => false], 404);
+    }
+
+    public function search(Request $request)
+    {
+        $query = $request->input('query');
+        $users = User::where('name', 'like', "%$query%")
+                    ->orWhere('email', 'like', "%$query%")
+                    ->paginate(10);
+
+        return view('admin.users.list',[
+            'title' => 'Kết quả tìm kiếm'
+        ], compact('users'))->with('query', $query);
+    }
+
+    public function editProfile()
+    {
+        $user = Auth::user();
+        return view('admin.users.profile',[
+            'title' => 'Thông tin người dùng'
+        ], compact('user'));
+    }
+
+    public function updateProfile(Request $request)
+    {
+    
+    
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'phone' => 'nullable|string|max:15',
+            'thumb' => 'nullable|string',
+        ]);
+    
+        $user = Auth::user();
+        $user->name = $request->name;
+        $user->phone = $request->phone;
+        if ($request->thumb) {
+            $user->thumb = $request->thumb;
+        }
+      
+        $user->save();
+        \Log::info($request->all());  // Kiểm tra dữ liệu gửi lên
+
+    
+        return redirect()->route('user.profile')->with('success', 'Profile updated successfully');
+    }
+}
