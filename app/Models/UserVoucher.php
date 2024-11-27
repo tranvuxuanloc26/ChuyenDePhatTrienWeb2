a1339312a2774df835431868974a2c069af177c3
PhamLeMinhTinh <21211tt1504@mail.tdc.edu.vn>
2024-11-27 19:34:20 +0700

    mã giam gia

---
+++ app/Models/UserVoucher.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserVoucher extends Model
+{
+    use HasFactory;
+
+    protected $table = 'user_vouchers';
+
+
+    protected $fillable = [
+        'user_id',
+        'voucher_id',
+        'status',
+        'created_at',
+        'updated_at',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function voucher()
+    {
+        return $this->belongsTo(Voucher::class, 'voucher_id');
+    }
+}
