b4d1149499ffc446aa60fcf47a4f3de0fb259e49
NguyenMaiThao <maithao123@gmail.com>
2024-11-28 09:42:21 +0700

    update src

--- resources/views/carts/list.blade.php
+++ resources/views/carts/list.blade.php
@@ -1,7 +1,7 @@
 @extends('main')
 
 @section('content')
-<form class="bg0 p-t-100 p-b-85" method="post">
+<form class="bg0 p-t-100 p-b-85" method="post" style="padding-bottom: 0">
     @include('admin.alert')
     @if(count($products) != 0)
 
@@ -10,68 +10,69 @@
             <div class="col-lg-10 col-xl-7 m-lr-auto m-b-50">
                 <div class="m-l-25 m-r--38 m-lr-0-xl">
                     <div class="wrap-table-shopping-cart">
-                        @php $total = 0;  @endphp
+                        @php $total = 0; @endphp
                         <table class="table-shopping-cart">
-                            <tbody><tr class="table_head">
-                                <th class="column-1">Product</th>
-                                <th class="column-2"></th>
-                                <th class="column-3">Price</th>
-                                <th class="column-4">Quantity</th>
-                                <th class="column-5">Total</th>
+                            <tbody>
+                                <tr class="table_head">
+                                    <th class="column-1">Product</th>
+                                    <th class="column-2"></th>
+                                    <th class="column-3">Price</th>
+                                    <th class="column-4">Quantity</th>
+                                    <th class="column-5">Total</th>
 
-                            </tr>
-                              @foreach ($products as $key => $product)
-                              @php
-                                  $price = $product->price_sale != 0 ? $product->price_sale : $product->price;
-                                  $priceEnd = $price * $carts[$product->id];
-                                  $total += $priceEnd;
-                              @endphp
-                              <tr class="table_row">
-                                <td class="column-1">
-                                    <div class="how-itemcart1">
-                                        <img src="{{ $product->thumb }}" alt="IMG">
-                                    </div>
-                                </td>
-                                <td class="column-2">{{ $product->name }}</td>
-                                <td class="column-3">{{ number_format($price, 0, '', '.') }}</td>
-                                <td class="column-4">
-                                    <div class="wrap-num-product flex-w m-l-auto m-r-0">
-                                        <div class="btn-num-product-down cl8 hov-btn3 trans-04 flex-c-m">
-                                            <i class="fs-16 zmdi zmdi-minus"></i>
+                                </tr>
+                                @foreach ($products as $key => $product)
+                                @php
+                                $price = $product->price_sale != 0 ? $product->price_sale : $product->price;
+                                $priceEnd = $price * $carts[$product->id];
+                                $total += $priceEnd;
+                                @endphp
+                                <tr class="table_row">
+                                    <td class="column-1">
+                                        <div class="how-itemcart1">
+                                            <img src="{{ $product->thumb }}" alt="IMG">
                                         </div>
+                                    </td>
+                                    <td class="column-2">{{ $product->name }}</td>
+                                    <td class="column-3">{{ number_format($price, 0, '', '.') }}</td>
+                                    <td class="column-4">
+                                        <div class="wrap-num-product flex-w m-l-auto m-r-0">
+                                            <div class="btn-num-product-down cl8 hov-btn3 trans-04 flex-c-m">
+                                                <i class="fs-16 zmdi zmdi-minus"></i>
+                                            </div>
 
-                                        <input class="mtext-104 cl3 txt-center num-product" type="number" name="num_product[{{ $product->id }}]" value="{{ $carts[$product->id]  }}">
+                                            <input class="mtext-104 cl3 txt-center num-product" type="number" name="num_product[{{ $product->id }}]" value="{{ $carts[$product->id]  }}">
 
-                                        <div class="btn-num-product-up cl8 hov-btn3 trans-04 flex-c-m">
-                                            <i class="fs-16 zmdi zmdi-plus"></i>
+                                            <div class="btn-num-product-up cl8 hov-btn3 trans-04 flex-c-m">
+                                                <i class="fs-16 zmdi zmdi-plus"></i>
+                                            </div>
                                         </div>
-                                    </div>
-                                </td>
-                                <td class="column-5"> {{ number_format($priceEnd, 0, '', '.') }}</td>
-                                <td class="p-r-15">
-                                    <a href="/carts/delete/{{ $product->id }}">Xoá</a>
-                                </td>
-                            </tr>
-                              @endforeach
-                           
+                                    </td>
+                                    <td class="column-5"> {{ number_format($priceEnd, 0, '', '.') }}</td>
+                                    <td class="p-r-15">
+                                        <a href="/carts/delete/{{ $product->id }}">Xoá</a>
+                                    </td>
+                                </tr>
+                                @endforeach
 
-                          
-                        </tbody>
-                     
-                    </table>
+
+
+                            </tbody>
+
+                        </table>
                     </div>
 
                     <div class="flex-w flex-sb-m bor15 p-t-18 p-b-15 p-lr-40 p-lr-15-sm">
                         <div class="flex-w flex-m m-r-20 m-tb-5">
                             <input class="stext-104 cl2 plh4 size-117 bor13 p-lr-20 m-r-10 m-tb-5" type="text" name="coupon" placeholder="Coupon Code">
-                                
+
                             <div class="flex-c-m stext-101 cl2 size-118 bg8 bor13 hov-btn3 p-lr-15 trans-04 pointer m-tb-5">
                                 Apply coupon
                             </div>
                         </div>
 
                         <input type="submit" value="Update Cart" formaction="/update-cart" class="flex-c-m stext-101 cl2 size-119 bg8 bor13 hov-btn3 p-lr-15 trans-04 pointer m-tb-10">
-                        
+
                         @csrf
                     </div>
                 </div>
@@ -108,7 +109,7 @@
                             {{-- <p class="stext-111 cl6 p-t-2">
                                 There are no shipping methods available. Please double check your address, or contact us if you need any help.
                             </p> --}}
-                            
+
                             <div class="p-t-15">
                                 <span class="stext-112 cl8">
                                     THÔNG TIN KHÁCH HÀNG
@@ -124,11 +125,11 @@
                                 </div> --}}
 
                                 <div class="bor8 bg0 m-b-12">
-                                    <input class="stext-111 cl8 plh3 size-111 p-lr-15" type="text" name="name" placeholder="Tên Khách Hàng" >
+                                    <input class="stext-111 cl8 plh3 size-111 p-lr-15" type="text" name="name" placeholder="Tên Khách Hàng">
                                 </div>
-                                 
+
                                 <div class="bor8 bg0 m-b-12">
-                                    <input class="stext-111 cl8 plh3 size-111 p-lr-15" type="text" name="phone" placeholder="Số Điện Thoại" >
+                                    <input class="stext-111 cl8 plh3 size-111 p-lr-15" type="text" name="phone" placeholder="Số Điện Thoại">
                                 </div>
 
                                 <div class="bor8 bg0 m-b-12">
@@ -142,10 +143,10 @@
                                 <div class="bor8 bg0 m-b-12">
                                     <textarea class="stext-111 cl8 plh3 size-111 p-lr-15" type="text" name="content" placeholder="Ghi Chú"></textarea>
                                 </div>
-                          
-                                
-                            
-                                    
+
+
+
+
                             </div>
                         </div>
                     </div>
@@ -164,39 +165,41 @@
                         </div>
                     </div>
 
-                        <button class="flex-c-m stext-101 cl0 size-116 bg3 bor14 hov-btn3 p-lr-15 trans-04 pointer">
-                            Thanh toán COD
-                        </button>
-                 
-                        
-                       <!-- Nút Thanh toán MoMo -->
-                        <button class="mt-3 flex-c-m stext-101 cl0 size-116 bg-danger bor14 hov-btn3 p-lr-15 trans-04 pointer">
-                            Thanh toán MoMo
-                        </button>
+                    <button class="flex-c-m stext-101 cl0 size-116 bg3 bor14 hov-btn3 p-lr-15 trans-04 pointer">
+                        Thanh toán COD
+                    </button>
 
-                        
-                        
-                        
 
-                    
+                    <!-- Nút Thanh toán MoMo -->
+                    <button class="mt-3 flex-c-m stext-101 cl0 size-116 bg-danger bor14 hov-btn3 p-lr-15 trans-04 pointer">
+                        Thanh toán MoMo
+                    </button>
+
+
+
+
+
+
                 </div>
             </div>
         </div>
-        
+
     </div>
-   
+
 </form>
-<form action="/payment"  method="POST" style="margin-left: 60%; margin-bottom: 20px">
+<form action="/payment" method="POST" style="margin-left: 978px; margin-bottom: 20px; width:285px !important;">
     @csrf
     <input type="text" hidden name="total" value="{{ $total }}">
-    <button type="submit"  id="vnpay-btn" name="redirect" class="mt-3 vnpay-button flex-c-m stext-101 cl0 size-116 bg-success bor14 hov-btn3 p-lr-15 trans-04 pointer">
+    <button type="submit" id="vnpay-btn" name="redirect" class="mt-3 vnpay-button flex-c-m stext-101 cl0 size-116 bg-success bor14 hov-btn3 p-lr-15 trans-04 pointer">
         Thanh toán VNPay
     </button>
 </form>
 
 
 @else
-<div class="text-center"><h2>Giỏ hàng trống</h2></div>
+<div class="text-center">
+    <h2>Giỏ hàng trống</h2>
+</div>
 @endif
 
 @endsection
